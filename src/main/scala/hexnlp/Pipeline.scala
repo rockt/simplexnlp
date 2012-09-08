add413ae30c51d4a2a4c30bb04a21b330ae4473d
Tim Rocktäschel <trocktae@informatik.hu-berlin.de>
2012-09-08 14:01:28 +0200

    refactoring

--- src/main/scala/hexnlp/Pipeline.scala
+++ src/main/scala/hexnlp/Pipeline.scala
@@ -5,87 +5,84 @@ import edu.uchsc.ccp.nlp.ei.mutation.MutationFinder
 import hexnlp.Util._
 import hexnlp.Implicits._
 
-//something that has all kinds of children
+//something that has children
 trait ParentOf[C <: Child] {
-  val children = new ListBuffer[C]
-  def add(child:C) = {
-    children += child
+  private val childrenBuffer = new ListBuffer[C]
+  def children = childrenBuffer.toList
+  def add(child: C) = {
+    childrenBuffer += child
     child.parent = this
   }
-  def remove(child:C) = {
-    children - child
+  def remove(child: C) = {
+    childrenBuffer - child
     child.parent = null
   }
-  def +(child:C) = add(child)
-  def -(child:C) = remove(child)
-  def filteredChildren[T](implicit mf:Manifest[T]):List[T] = {
-     children.collect({
-       case t if mf.erasure.isAssignableFrom(t.getClass) => t
-     }).asInstanceOf[ListBuffer[T]].toList
-   }
+  def +(child: C) = add(child)
+  def -(child: C) = remove(child)
+  //gathers all descendants recursively
+  def descendants: List[C] = {
+    val buffer = new ListBuffer[C]
+    for (child <- this.asInstanceOf[ParentOf[C]].childrenBuffer) {
+      buffer.append(child)
+      //recursion
+      if (child.isInstanceOf[ParentOf[C]]) buffer.appendAll(child.asInstanceOf[ParentOf[C]].descendants)
+    }
+    buffer.toList
+  }
+  def childrenFilterBy[T](implicit mf: Manifest[T]) = filterByType[T](children)
+  def descendantsFilterBy[T](implicit mf: Manifest[T]) = filterByType[T](descendants)
 }
 
 //something that has a parent
 trait Child {
-  var parent:Any = _
+  var parent: Any = _
 }
 
 trait Annotation extends Child {
-  def doc:Document = {
+  def doc: Document = {
     if (parent.isInstanceOf[Document]) parent.asInstanceOf[Document]
     else parent.asInstanceOf[Annotation].doc
-  }
-  //gathers all annotations recursively
-  def annotations:List[Annotation] = {
-    //if this is a parent, collect all children
-    if (this.isInstanceOf[ParentOf[Annotation]]) {
-      val buffer = new ListBuffer[Annotation]
-      buffer.append(this)
-      for (child <- this.asInstanceOf[ParentOf[Annotation]].children) {
-        buffer.appendAll(child.annotations)
-      }
-      buffer.toList
-    } else List(this)
-
   }
 }
 
 //TODO: every document needs an ID
-//a document with mutable annotations
-class Document(val text:String) extends Annotation with ParentOf[Annotation] {
+//a document that store annotations
+class Document(val text: String) extends Annotation with ParentOf[Annotation] {
   override def doc = this
-  def sentences = filteredChildren[Sentence]
+  def sentences = childrenFilterBy[Sentence]
 }
 
 //TODO: implement a Parameter class for components
 //a NLP component
 abstract class Component {
-  def initialize(){}
-  def process(doc:Document) //a concrete component needs to override this method
-  def preHook(){}
-  def postHook(){}
+  def initialize() {}
+  def process(doc: Document)
+  //a concrete component needs to override this method
+  def preHook() {}
+  def postHook() {}
   initialize()
 }
 
 //a chain of NLP components
-class Pipeline(cs:Component*) {
+class Pipeline(cs: Component*) {
   val components = new ListBuffer[Component]
-  components.appendAll(cs) //FIXME: that is silly
-  def ++(that:Pipeline) = new Pipeline((this.components ++ that.components): _*)
+  components.appendAll(cs)
+  //FIXME: that is silly
+  def ++(that: Pipeline) = new Pipeline((this.components ++ that.components): _*)
   //TODO: def |(that:Pipeline)
-  def process(doc:Document) = {
-    components.foreach((c:Component) => {
+  def process(doc: Document) = {
+    components.foreach((c: Component) => {
       c.preHook()
       c.process(doc)
       c.postHook()
-      })
+    })
   }
   override def toString = components.map(getClassName(_)).mkString("*Input* => ", " -> ", " => *Output*")
 }
 
 trait Span extends Annotation {
-  val start:Int
-  val end:Int
+  val start: Int
+  val end: Int
   assert(end - start >= 0, "A span must start before it ends!")
   //TODO: def append
   //TODO: def prepend
@@ -97,25 +94,27 @@ trait Span extends Annotation {
 }
 
 trait NonOverlappingSpan extends Span with Ordered[NonOverlappingSpan] {
-  override def compare(that:NonOverlappingSpan):Int = {
+  override def compare(that: NonOverlappingSpan): Int = {
     assert((this.start < that.start || this.start > that.start) && (this.end <= that.start || this.start >= that.end), this + " overlaps " + that)
     this.start - that.start
   }
 }
 
-case class Sentence(start:Int, end:Int) extends NonOverlappingSpan with ParentOf[Span] {
-  def tokens = filteredChildren[Token]
-  def entities = filteredChildren[Entity]
-  def genes = filteredChildren[Gene]
-  def mutations = filteredChildren[Mutation]
-  def diseases = filteredChildren[Disease]
+case class Sentence(start: Int, end: Int) extends NonOverlappingSpan with ParentOf[Span] {
+  def tokens = childrenFilterBy[Token]
+  def entities = childrenFilterBy[Entity]
+  def genes = childrenFilterBy[Gene]
+  def mutations = childrenFilterBy[Mutation]
+  def diseases = childrenFilterBy[Disease]
 }
 
-case class Token(start:Int, end:Int) extends NonOverlappingSpan with Child { var pos:String = _ }
+case class Token(start: Int, end: Int) extends NonOverlappingSpan {
+  var pos: String = _
+}
 
-trait Entity extends Span with Child
+trait Entity extends Span
 
-abstract class Relation(entities:Entity*) extends Span {
+abstract class Relation(entities: Entity*) extends Span {
   //TODO: find start of first and end of last entity instead
   val start = entities.head.start
   val end = entities.head.end
@@ -123,19 +122,19 @@ abstract class Relation(entities:Entity*) extends Span {
 }
 
 //Example NLP pipeline
-case class Mutation(start:Int, end:Int) extends Entity
-case class Gene(start:Int, end:Int) extends Entity
-case class Disease(start:Int, end:Int) extends Entity
-case class Drug(start:Int, end:Int) extends Entity
+case class Mutation(start: Int, end: Int) extends Entity
+case class Gene(start: Int, end: Int) extends Entity
+case class Disease(start: Int, end: Int) extends Entity
+case class Drug(start: Int, end: Int) extends Entity
 
-case class PPI(a:Gene, b:Gene) extends Relation(a, b) {
-  override def toString:String = "PPI: " + a + " - " + b
+case class PPI(a: Gene, b: Gene) extends Relation(a, b) {
+  override def toString: String = "PPI: " + a + " - " + b
 }
-case class DDI(a:Drug, b:Drug) extends Relation(a, b) {
-  override def toString:String = "DDI: " + a + " - " + b
+case class DDI(a: Drug, b: Drug) extends Relation(a, b) {
+  override def toString: String = "DDI: " + a + " - " + b
 }
-case class MutationDiseaseRelation(m:Mutation, d:Disease) extends Relation(m, d) {
-  override def toString:String = "MutationDiseaseRelation: " + m + " - " + d
+case class MutationDiseaseRelation(m: Mutation, d: Disease) extends Relation(m, d) {
+  override def toString: String = "MutationDiseaseRelation: " + m + " - " + d
 }
 
 class DummyDiseaseAnnotator extends Component {
@@ -153,27 +152,26 @@ class DummySentenceAnnotator extends Component {
 }
 
 class MutationAnnotator extends Component {
-  var extractor:MutationFinder  = _
+  var extractor: MutationFinder = _
   override def initialize() {
     suppressConsoleOutput {
       extractor = new MutationFinder("./resources/mutationFinder/regex.txt")
     }
   }
-
-  override def process(doc:Document) = {
+  override def process(doc: Document) = {
     import scala.collection.JavaConversions._
     for (sentence <- doc.sentences) {
       val mutations = extractor.extractMutations(sentence.text)
-        for (mutation <- mutations.keySet(); tuple <- mutations.get(mutation)) {
-          val span = tuple.asInstanceOf[Array[Int]]
-          sentence + Mutation(span(0), span(1))
-        }
+      for (mutation <- mutations.keySet(); tuple <- mutations.get(mutation)) {
+        val span = tuple.asInstanceOf[Array[Int]]
+        sentence + Mutation(span(0), span(1))
+      }
     }
   }
 }
 
 class CoOccurrenceAnnotator extends Component {
-  override def process(doc:Document) = {
+  override def process(doc: Document) = {
     //TODO
   }
 }
@@ -189,10 +187,10 @@ object Prototype extends App {
   println("Text:\t\t" + doc.text)
   println("Sentences:")
   println(doc.sentences)
-  println("Sentence annotations")
+  println("Sentence descendants")
   for (sentence <- doc.sentences) {
-    println(sentence.annotations)
+    println(sentence.descendants)
   }
   println("All Annotations:")
-  println(doc.annotations)
+  println(doc.descendants)
 }
--- src/main/scala/hexnlp/Util.scala
+++ src/main/scala/hexnlp/Util.scala
@@ -9,11 +9,14 @@ object Util {
     body
     System.setErr(temp)
   }
-
   def getClassName(a:Any) = a.getClass.toString.substring(a.getClass.toString.lastIndexOf('.') + 1)
-
+  def filterByType[T](list:List[Any])(implicit mf:Manifest[T]):List[T] = {
+    list.collect({
+      case t if mf.erasure.isAssignableFrom(t.getClass) => t
+    }).asInstanceOf[List[T]]
+  }
   //TODO: use currying if applicable
-  //TODO: getAnnotationsWithin(start: Int, end: Int)(annotations:Iterable[Span])
+  //TODO: getAnnotationsWithin(start: Int, end: Int)(descendants:Iterable[Span])
   //TODO: getAnnotationsStartingAt(start: Int)
   //TODO: getAnnotationsStartingAfter(start: Int)
   //TODO: getAnnotationsEndingAt(end: Int)
